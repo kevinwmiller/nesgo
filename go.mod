a88054c50028f96e1cd0d771961999686bdfbbcb
Kevin Miller <kevin@kevinmiller.dev>
2019-11-20 21:58:26 -0500

    Added empty opengl window

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module nesgo
 go 1.13
 
 require (
-	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7 // indirect
+	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7
 	github.com/go-gl/glfw v0.0.0-20190409004039-e6da0acd62b1
 	github.com/stretchr/testify v1.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"nesgo/nes/bus"
 	"nesgo/nes/clock"
 	"nesgo/nes/cpu6502"
+	"runtime"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.1/glfw"
 )
 
 func main() {
@@ -14,27 +19,33 @@ func main() {
 	clock.RegisterComponent(cpu, 3)
 	cpu.Dump()
 
-	// runtime.LockOSThread()
-	// if err := glfw.Init(); err != nil {
-	// 	panic(fmt.Errorf("could not initialize glfw: %v", err))
-	// }
+	runtime.LockOSThread()
+	if err := glfw.Init(); err != nil {
+		panic(fmt.Errorf("could not initialize glfw: %v", err))
+	}
 
-	// glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	// glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	// glfw.WindowHint(glfw.Resizable, glfw.True)
-	// glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	// glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.Resizable, glfw.True)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 
-	// win, err := glfw.CreateWindow(800, 600, "Hello world", nil, nil)if err != nil {
-	// 	panic(fmt.Errorf("could not create opengl renderer: %v", err))
-	// }
+	win, err := glfw.CreateWindow(800, 600, "Nesgo", nil, nil)
+	if err != nil {
+		panic(fmt.Errorf("could not create opengl renderer: %v", err))
+	}
 
-	// win.MakeContextCurrent()
+	win.MakeContextCurrent()
 
-	// for !win.ShouldClose() {
-	// 	win.SwapBuffers()
-	// 	glfw.PollEvents()
-	//  }
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	gl.ClearColor(0, 0.5, 1.0, 1.0)
+	for !win.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		win.SwapBuffers()
+		glfw.PollEvents()
+	}
 
 	// clock.Start()
 }
